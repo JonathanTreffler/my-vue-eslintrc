34ccf538046b43afda293ffdb110f5389d41d1f6
Jonathan Treffler <mail@jonathan-treffler.de>
2020-09-02 21:21:54 +0200

    Update .eslintrc.js

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,5 +33,19 @@ module.exports = {
 			"always"
 		],
 		"vue/no-use-v-if-with-v-for": ["warn"],
+		'comma-dangle': [ 'error', {
+			arrays: 'always',
+			objects: 'always',
+			imports: 'never',
+			exports: 'never',
+			functions: 'never',
+		}, ],
+		semi: [ 'error', 'always', ],
+		'vue/html-indent': [ 'error', 'tab', ],
+		'no-tabs': 0,
+		'array-bracket-spacing': [ 'error', 'always', ],
+		'comma-style': [ 'error', 'last', ],
+		'comma-spacing': [ 'error', { before: false, after: true, }, ],
+		'space-before-function-paren': [ 'error', 'never', ],
 	}
 };
